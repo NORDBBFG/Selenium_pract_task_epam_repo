6375fb45ca0047dd9f2f67badfd673d103d34430
NORDBBFG <nbasisty911@gmail.com>
2024-12-02 14:26:59 +0200

    Added headless run function

---
+++ Selenium_pract_task/FileHelper/EnvironmentVariableHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace Selenium_pract_task.FileHelper
+{
+    public static class EnvironmentVariableHelper
+    {
+        public static void EnsureEnvironmentVariableExist(string environmentVariableName, string environmentVariableState)
+        {
+            var headlessValue = Environment.GetEnvironmentVariable(environmentVariableName);
+
+            if (string.IsNullOrEmpty(headlessValue))
+            {
+                Environment.SetEnvironmentVariable(environmentVariableName, environmentVariableState, EnvironmentVariableTarget.Process);
+            }
+        }
+    }
+}
