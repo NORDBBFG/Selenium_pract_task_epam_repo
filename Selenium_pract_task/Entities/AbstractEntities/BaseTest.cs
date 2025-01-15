a471f401d3cdcf7d47c2ff57c824985bdb34509b
NORDBBFG <nbasisty911@gmail.com>
2025-01-15 10:45:14 +0200

    Delete dummy variable

--- Selenium_pract_task/Entities/AbstractEntities/BaseTest.cs
+++ Selenium_pract_task/Entities/AbstractEntities/BaseTest.cs
@@ -15,7 +15,6 @@ namespace Selenium_pract_task.Entities.AbstractEntities
         private IConfiguration _browserConfig;
         private readonly string baseUrl = "https://www.epam.com";
         private string headlesRunStatus = "true";
-        private string dummy;
 
         [SetUp]
         public void Setup()
